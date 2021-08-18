ceb25dd9bb3788b1e4e33dd5e2359944f2a5338a
philiwon8868 <wongp2009@gmail.com>
2021-08-18 14:36:06 +0800

    commit

--- main.tf
+++ main.tf
@@ -587,6 +587,16 @@ resource "fmc_port_objects" "ssh" {
     protocol = "TCP"
 }
 
+resource "fmc_policy_devices_assignments" "policy_assignment" {
+    policy {
+        id = fmc_access_policies.acp.id
+        type = fmc_access_policies.acp.type
+    }
+    target_devices {
+        id = data.fmc_devices.device.id
+        type = data.fmc_devices.device.type
+    }
+}
 resource "fmc_access_rules" "access_rule" {
     acp = data.fmc_access_policies.acp.id
     section = "mandatory"
