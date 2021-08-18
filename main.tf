8e208218ce78b65386ac0a10951bc526f8a5950d
philiwon8868 <wongp2009@gmail.com>
2021-08-18 14:01:10 +0800

    c

--- main.tf
+++ main.tf
@@ -601,7 +601,7 @@ resource "fmc_access_rules" "access_rule" {
     log_end = each.value.log_end
     source_zones {
         source_zone {
-            id = ${"data.fmc_security_zones.inside.id"}
+            id = data.fmc_security_zones.${each.value.source_zone}.id
             type =  "data.fmc_security_zones.${each.value.source_zone}.type"
         }
     }
